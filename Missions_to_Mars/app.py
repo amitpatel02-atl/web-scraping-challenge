6dd2302c4eed94c0a6a4018e2ea7672c78fe8f9a
amitpatel02-atl <61107007+amitpatel02-atl@users.noreply.github.com>
2020-06-07 22:48:07 -0400

    Update code

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def index():
 
     # Find one record of data from the mongo database
-    mars = mongo.db.mars.find_one()
+    mars = mongo.db.collection.find_one()
 
     # Return template and data
     return render_template("index.html", mars=mars)
@@ -24,13 +24,15 @@ def index():
 def scrape():
 
     # Create a mongo database
-    mars = mongo.db.mars
+    mars_db= mongo.db.mars
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape_all()
+
+    print(mars_data)
 
     # Update the Mongo database using update and upsert=True
-    mars.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
